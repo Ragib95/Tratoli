d8ddeaafe446238782d593f0e7ef5971f6246615
Md Dilnawaz Ragib <dilnawazragib@gmail.com>
2017-04-16 10:39:34 +0530

    Add files via upload
    Chatapp

---
+++ chatapp/chatroom/views.py
@@ -0,0 +1,59 @@
+from django.shortcuts import render, render_to_response
+from django.contrib.auth import authenticate, logout, login
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from chatapp import settings
+
+from .models import Chat
+from chatroom.form import *
+from django.template import RequestContext
+
+
+def register_page(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = User.objects.create_user(username=form.cleaned_data['username'],password=form.cleaned_data['password1'],email=form.cleaned_data['email'])
+            return HttpResponseRedirect('/')
+    form = RegistrationForm()
+    variables = RequestContext(request, {'form': form})
+    return render('chatroom/register.html', variables)
+
+
+def Login(request):
+    next = request.GET.get('next', '/home/')
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect(next)
+            else:
+                return HttpResponse("Account is not active at the moment.")
+        else:
+            return HttpResponseRedirect(settings.LOGIN_URL)
+    return render(request, "chatroom/login.html", {'next': next})
+
+def Logout(request):
+    logout(request)
+    return HttpResponseRedirect('/login/')
+
+def Home(request):
+    c = Chat.objects.all()
+    return render(request, "chatroom/home.html", {'home': 'active', 'chat': c})
+
+def Post(request):
+    if request.method == "POST":
+        msg = request.POST.get('msgbox', None)
+        c = Chat(user=request.user, message=msg)
+        if msg != '':
+            c.save()
+        return JsonResponse({ 'msg': msg, 'user': c.user.username })
+    else:
+        return HttpResponse('Request must be POST.')
+
+def Messages(request):
+    c = Chat.objects.all()
+    return render(request, 'chatroom/messages.html', {'chat': c})
